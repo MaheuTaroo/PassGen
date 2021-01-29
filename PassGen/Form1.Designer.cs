fe43c5fc0625052936bc8e5b9978942f47d6ea2e
MaheuTaroo <pantupa@gmail.com>
2021-01-29 22:00:37 +0000

    Initial commit

---
+++ PassGen/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace PassGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nupPassCount = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chkSymbols = new System.Windows.Forms.CheckBox();
+            this.chkNumbers = new System.Windows.Forms.CheckBox();
+            this.chkLetters = new System.Windows.Forms.CheckBox();
+            this.lbPasswords = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nupCharCount = new System.Windows.Forms.NumericUpDown();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.nupPassCount)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupCharCount)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Passwords to generate:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Characters:";
+            // 
+            // nupPassCount
+            // 
+            this.nupPassCount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nupPassCount.Location = new System.Drawing.Point(223, 31);
+            this.nupPassCount.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupPassCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nupPassCount.Name = "nupPassCount";
+            this.nupPassCount.Size = new System.Drawing.Size(44, 26);
+            this.nupPassCount.TabIndex = 3;
+            this.nupPassCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.chkSymbols);
+            this.panel1.Controls.Add(this.chkNumbers);
+            this.panel1.Controls.Add(this.chkLetters);
+            this.panel1.Location = new System.Drawing.Point(197, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(96, 66);
+            this.panel1.TabIndex = 4;
+            // 
+            // chkSymbols
+            // 
+            this.chkSymbols.AutoSize = true;
+            this.chkSymbols.Location = new System.Drawing.Point(3, 39);
+            this.chkSymbols.Name = "chkSymbols";
+            this.chkSymbols.Size = new System.Drawing.Size(88, 24);
+            this.chkSymbols.TabIndex = 2;
+            this.chkSymbols.Text = "Symbols";
+            this.chkSymbols.UseVisualStyleBackColor = true;
+            this.chkSymbols.CheckedChanged += new System.EventHandler(this.Chars);
+            // 
+            // chkNumbers
+            // 
+            this.chkNumbers.AutoSize = true;
+            this.chkNumbers.Location = new System.Drawing.Point(3, 21);
+            this.chkNumbers.Name = "chkNumbers";
+            this.chkNumbers.Size = new System.Drawing.Size(92, 24);
+            this.chkNumbers.TabIndex = 1;
+            this.chkNumbers.Text = "Numbers";
+            this.chkNumbers.UseVisualStyleBackColor = true;
+            this.chkNumbers.CheckedChanged += new System.EventHandler(this.Chars);
+            // 
+            // chkLetters
+            // 
+            this.chkLetters.AutoSize = true;
+            this.chkLetters.Location = new System.Drawing.Point(3, 3);
+            this.chkLetters.Name = "chkLetters";
+            this.chkLetters.Size = new System.Drawing.Size(78, 24);
+            this.chkLetters.TabIndex = 0;
+            this.chkLetters.Text = "Letters";
+            this.chkLetters.UseVisualStyleBackColor = true;
+            this.chkLetters.CheckedChanged += new System.EventHandler(this.Chars);
+            // 
+            // lbPasswords
+            // 
+            this.lbPasswords.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbPasswords.FormattingEnabled = true;
+            this.lbPasswords.HorizontalScrollbar = true;
+            this.lbPasswords.ItemHeight = 20;
+            this.lbPasswords.Location = new System.Drawing.Point(3, 3);
+            this.lbPasswords.Name = "lbPasswords";
+            this.lbPasswords.Size = new System.Drawing.Size(181, 267);
+            this.lbPasswords.TabIndex = 5;
+            this.lbPasswords.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PassSelect);
+            this.lbPasswords.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PassCopy);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 214);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password length:";
+            // 
+            // nupCharCount
+            // 
+            this.nupCharCount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nupCharCount.Location = new System.Drawing.Point(222, 211);
+            this.nupCharCount.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.nupCharCount.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.nupCharCount.Name = "nupCharCount";
+            this.nupCharCount.Size = new System.Drawing.Size(46, 26);
+            this.nupCharCount.TabIndex = 6;
+            this.nupCharCount.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGenerate.Location = new System.Drawing.Point(111, 287);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(87, 30);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.GeneratePasswords);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label5.Location = new System.Drawing.Point(3, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(180, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Double-click will copy password";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 154);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Passwords:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.78839F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.2116F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(319, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(291, 328);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.lbPasswords, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(101, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.98402F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.01597F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(187, 322);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.65563F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.34437F));
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel5, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel1, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(613, 334);
+            this.tableLayoutPanel3.TabIndex = 11;
+            // 
+            // tableLayoutPanel5
+            // 
+            this.tableLayoutPanel5.ColumnCount = 1;
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel5.Controls.Add(this.btnGenerate, 0, 1);
+            this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            this.tableLayoutPanel5.RowCount = 2;
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.45122F));
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.54878F));
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(310, 328);
+            this.tableLayoutPanel5.TabIndex = 12;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.67883F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.32117F));
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.nupPassCount, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.nupCharCount, 1, 2);
+            this.tableLayoutPanel4.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel4.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 3;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(304, 270);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 334);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(629, 373);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PassGen";
+            ((System.ComponentModel.ISupportInitialize)(this.nupPassCount)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupCharCount)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkSymbols;
+        private System.Windows.Forms.CheckBox chkNumbers;
+        private System.Windows.Forms.CheckBox chkLetters;
+        private System.Windows.Forms.ListBox lbPasswords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nupCharCount;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.NumericUpDown nupPassCount;
+    }
+}
+
---
+++ PassGen/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+namespace PassGen
+{
+    public partial class Form1 : Form
+    {
+        const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", numbers = "1234567890", symbols = "!\"#$%&/()=?»@£§€{[]}«+*-<>\\|;:_";
+        string conjunction = "", final = "";
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Icon = Properties.Resources._lock;
+            chkLetters.Checked = true;
+        }
+        private void Chars(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked) count++;
+            else count--;
+
+            if (count == 0) ((CheckBox)sender).Checked = true;
+        }
+        private void PassSelect(object sender, MouseEventArgs e)
+        {
+            if (lbPasswords.IndexFromPoint(e.Location) == -1) lbPasswords.SelectedIndex = -1;
+        }
+        private void PassCopy(object sender, MouseEventArgs e)
+        {
+            if (lbPasswords.SelectedIndex != -1) Clipboard.SetText(lbPasswords.Items[lbPasswords.SelectedIndex].ToString());
+            MessageBox.Show("The password was copied to your clipboard!", "Password copied", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void GeneratePasswords(object sender, EventArgs e)
+        {
+            lbPasswords.Items.Clear();
+            Random r = new Random();
+            foreach (CheckBox chk in panel1.Controls)
+            {
+                if (chk.Checked)
+                {
+                    conjunction += chk.Text == "Letters" ? letters : chk.Text == "Numbers" ? numbers : symbols;
+                }
+            }
+            for (int pass = 0; pass < nupPassCount.Value; pass++)
+            {
+                for (int character = 0; character < nupCharCount.Value; character++)
+                {
+                    final += conjunction[r.Next(0, conjunction.Length)];
+                }
+                lbPasswords.Items.Add(final);
+                final = "";
+            }
+            conjunction = "";
+        }
+    }
+}
